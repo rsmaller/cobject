c685a43223f5ef3f024ece4157b139a6dbcf299e
Ryan Smaller <ryjobobo@gmail.com>
2025-05-06 19:44:18 -0400

    Added new methods and improved IDing objects

--- object.c
+++ object.c
@@ -1,4 +1,4 @@
-#if !defined(__GNUC__) && !defined(__clang__)
+#if !defined(__GNUC__) && !defined(__clang__) && !defined(__INTELLISENSE__)
     #error This program uses GCC/Clang extensions. Please use a compatible compiler.
 #endif
 
@@ -7,6 +7,8 @@
 #include <string.h>
 
 #define concat(x, y) x ## y
+
+#define strTokenize(x) # x
 
 #define expand(x) x
 
@@ -32,25 +34,42 @@
         function(__VA_ARGS__, arg1Name);\
     }
 
+#define double_expanded_decorate_void(function, identifier, arg1Type, arg1Name, arg2Type, arg2Name, ...)\
+    void expand(concat(function, identifier)) (expand(arg1Type) arg1Name, expand(arg2Type) arg2Name) {\
+        function(__VA_ARGS__, arg1Name, arg2Name);\
+    }
+
 #ifdef __INTELLISENSE__
-    #define constructObject(arg1, arg2) *construct_object_internal(arg1) 
+    #define constructObject(arg1) *internalObjectAllocator(arg1) 
 #else // prevents IntelliSense from complaining about the GCC-specific macro
-    #define constructObject(arraySize, identifier)\
-    ({\
-        object returnObject = *(construct_object_internal(arraySize));\
+    #define constructObjectInternal(arraySize, identifier) ({\
+        object returnObject = *(internalObjectAllocator(arraySize));\
         decorate(size_t, lengthFunc, identifier, &returnObject);\
         expanded_decorate_void(resizeFunc, identifier, size_t, newArraySize, &returnObject);\
+        expanded_decorate(int *, atFunc, identifier, size_t, index, &returnObject);\
+        expanded_decorate(int, getFunc, identifier, size_t, index, &returnObject);\
+        double_expanded_decorate_void(setFunc, identifier, size_t, index, int, value, &returnObject);\
+        returnObject.id = strTokenize(identifier);\
         returnObject.length = expand(concat(lengthFunc, identifier));\
         returnObject.resize = expand(concat(resizeFunc, identifier));\
+        returnObject.at = expand(concat(atFunc, identifier));\
+        returnObject.get = expand(concat(getFunc, identifier));\
+        returnObject.set = expand(concat(setFunc, identifier));\
         returnObject;\
     })
+
+    #define constructObject(arraySize) constructObjectInternal(arraySize, __COUNTER__)
 #endif
 
 typedef struct object {
+    char *id;
     int *array;
     size_t size;
     size_t (*length)(void);
-    void (*resize)(size_t);
+    void (*resize)(size_t newArraySize);
+    int *(*at)(size_t index);
+    void (*set)(size_t index, int value);
+    int (*get)(size_t index);
 } object;
 
 size_t lengthFunc(object *self) {
@@ -60,18 +79,44 @@ size_t lengthFunc(object *self) {
 void resizeFunc(object *self, size_t newArraySize) {
     self -> size = newArraySize;
     self -> array = (int *)realloc(self -> array, newArraySize);
+    return;
 }
 
-object *construct_object_internal(size_t arraySize) {
+int *atFunc(object *self, size_t index) {
+    if (index >= self -> size) {
+        fprintf(stderr, "Index out of bounds. Terminating");
+        exit(1);
+    }
+    return self -> array + index;
+}
+
+int getFunc(object *self, size_t index) {
+    return *atFunc(self, index);
+}
+
+void setFunc(object *self, size_t index, int value) {
+    *atFunc(self, index) = value;
+    return;
+}
+
+object *internalObjectAllocator(size_t arraySize) {
     object *returnValue = (object *)malloc(sizeof(object));
-    returnValue -> array = (int *)malloc(arraySize * sizeof(int));
+    returnValue -> array = (int *)calloc(arraySize, sizeof(int));
     returnValue -> size = arraySize;
     return returnValue;
 }
 
 int main() {
-    object myObject = constructObject(100, 1);
-    printf("%llu\n", myObject.length());
+    object myObject = constructObject(100);
+    printf("Object %s size: %llu\n", myObject.id, myObject.length());
     myObject.resize(150);
-    printf("%llu\n", myObject.length());
+    printf("Object %s size: %llu\n", myObject.id, myObject.length());
+    object myObject2 = constructObject(200);
+    printf("Object %s size: %llu\n", myObject2.id, myObject2.length());
+    myObject2.resize(250);
+    printf("Object %s size: %llu\n", myObject2.id, myObject2.length());
+    printf("Object %s size: %llu\n", myObject.id, myObject.length());
+    printf("Object %s at index 0: %d\n", myObject.id, myObject.get(0));
+    myObject.set(0, 1);
+    printf("Object %s at index 0: %d\n", myObject.id, myObject.get(0));
 }
